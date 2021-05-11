3be64ce3e49bf717901ec20dbe39761f3c67030c
zhong <afs784512@hotmail.com>
2021-05-11 23:39:04 +0900

    首頁 Layout(navbar)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

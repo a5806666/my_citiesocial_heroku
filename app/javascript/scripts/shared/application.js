9b9556fb7de928ba2afca1bf289915f1acc4b65f
zhong <afs784512@hotmail.com>
2021-05-12 17:39:06 +0900

    安裝 Devise 及客制化登入頁面 part 2(notice/ navbar)

--- app/javascript/scripts/shared/application.js
+++ app/javascript/scripts/shared/application.js
@@ -24,3 +24,13 @@ document.addEventListener('turbolinks:load', function () {
     }
   
   });
+
+  document.addEventListener('turbolinks:load', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });

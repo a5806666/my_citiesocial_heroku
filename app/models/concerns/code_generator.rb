fd468ef4b254b93a5faf23b7a1ff370e69ad54c4
zhong <afs784512@hotmail.com>
2021-05-14 17:07:54 +0900

    使用 Concerns 簡化程式碼

---
+++ app/models/concerns/code_generator.rb
@@ -0,0 +1,14 @@
+module CodeGenerator
+    extend ActiveSupport::Concern
+  
+    included do
+      extend FriendlyId
+      friendly_id :code_generator, use: :slugged, slug_column: :code
+    end
+  
+    private
+  
+    def code_generator
+      SecureRandom.hex(10)
+    end
+  end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,8 @@
 class Product < ApplicationRecord
+  include CodeGenerator
+
   belongs_to :vendor
   validates :name, presence: true
   validates :list_price, :sell_price, numericality: { greater_than: 0, allow_nil: true }
-
-  # FriendlyId
-  extend FriendlyId
-  friendly_id :code_generator, use: :slugged, slug_column: :code
-
   validates :code, uniqueness: true
-  private
-  def code_generator
-      SecureRandom.hex(10)
-  end
 end

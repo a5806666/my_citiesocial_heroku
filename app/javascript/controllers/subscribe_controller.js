bb6a2987ba6533e287ccc8307d9d1f8fecbcc879
zhong <afs784512@hotmail.com>
2021-05-18 15:20:17 +0900

    電子報訂閱 part 2(subscribe.html / js/ model / controller)

--- app/javascript/controllers/subscribe_controller.js
+++ app/javascript/controllers/subscribe_controller.js
@@ -9,14 +9,30 @@ export default class extends Controller {
     let email = this.emailTarget.value.trim();
     // console.log(email)
     let data = new FormData();
-    data.append("subscribe['email']", email);
-    console.log(data)
+    data.append("subscribe[email]", email);
+    // console.log(data)
 
     Rails.ajax({
         url: '/api/v1/subscribe',
+        data: data,
         type: 'POST',
         dataType: 'json',
-        data: data,
+        success: (response) => {
+            // console.log(response)
+            switch (response.status) {
+                case "ok":
+                    alert('購読登録をしました!!!');
+                    this.emailTarget.value = '';
+                    break;
+            
+                case "duplicated":
+                    alert(`${response.email} すでに購読済み!!`);
+                    break;
+            }
+        },
+        error: function(err){
+            console.log(err);
+        }
     })
   }
 }
---
+++ spec/factories/subscribes.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :subscribe do
+    email { "MyString" }
+  end
+end

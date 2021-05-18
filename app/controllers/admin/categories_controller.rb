d471b79189be1d536cd5ee9deffc402e5f4ce016
zhong <afs784512@hotmail.com>
2021-05-18 19:00:23 +0900

    商品分類 part 3(category_edit/destroy)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CategoriesController < Admin::BaseController
+    before_action :category_find, only: [:edit, :update, :destroy]
+
     def index
-        
+        @categories = Category.all.order(position: :asc)
     end
 
     def new
@@ -17,8 +19,29 @@ class Admin::CategoriesController < Admin::BaseController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @category.update(category_params)
+            redirect_to edit_admin_category_path(@category), notice: 'ジャンルを更新しました！'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        # @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to admin_categories_path, notice: 'ジャンルを削除しました!!'
+    end
+
     private
     def category_params
         params.require(:category).permit(:name)
+    end
+
+    def category_find
+        @category = Category.find(params[:id])
     end
 end

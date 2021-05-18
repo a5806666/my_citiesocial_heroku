3d5e2fc86218d405251c57c6281a28055a78ef98
zhong <afs784512@hotmail.com>
2021-05-18 11:42:56 +0900

    商品庫存 part 4(Nested form)

--- app/javascript/controllers/product_form_controller.js
+++ app/javascript/controllers/product_form_controller.js
@@ -4,12 +4,22 @@ export default class extends Controller {
   static targets = [ "template", "link" ]
 
   add_sku(event) {
-        event.preventDefault();
+    event.preventDefault();
 
-        let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
-        // console.log(content)
-        this.linkTarget.insertAdjacentHTML('beforebegin', content);
+    let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    // console.log(content)
+    this.linkTarget.insertAdjacentHTML('beforebegin', content);
   }
-  connect() {
+  remove_sku(event) {
+    event.preventDefault();
+    // console.log('remove')
+    let wrapper = event.target.closest('.nested-fields')
+    // console.log(wrapper)
+    if (wrapper.dataset.newRecord == 'true') {
+      wrapper.remove();
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none'
+    }
   }
 }

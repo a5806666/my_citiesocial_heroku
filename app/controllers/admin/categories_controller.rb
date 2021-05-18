604a5122fd1950f5179071b1ebf13a72d61c9e0a
zhong <afs784512@hotmail.com>
2021-05-18 18:23:21 +0900

    商品分類 part 2(category_new/create gem_acts_as_list)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,23 @@ class Admin::CategoriesController < Admin::BaseController
     def index
         
     end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to admin_categories_path, notice: 'ジャンルを追加しました！'
+        else
+            render :new
+        end
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
     acts_as_paranoid
+    acts_as_list 
+    
     has_many :products
 
     validates :name, presence: true

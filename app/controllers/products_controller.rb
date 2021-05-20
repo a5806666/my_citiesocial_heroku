977992aab3f5c233ac3b97caa15b655c2a2a3048
zhong <afs784512@hotmail.com>
2021-05-20 15:51:47 +0900

    product_list(検索条件を変更する=> on_sell: :true)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
     def index
         @products = Product.order(updated_at: :desc).includes(:vendor)
+        @products = Product.where(on_sell: :true)
     end
 
     def show

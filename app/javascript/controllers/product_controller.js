f22566f999d5da909e53b29a4c4e3464b43d14da
zhong <afs784512@hotmail.com>
2021-05-23 18:07:02 +0900

    使用購物車(カート)  part 2(Rails.ajax)

--- app/javascript/controllers/product_controller.js
+++ app/javascript/controllers/product_controller.js
@@ -1,7 +1,39 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
-    static targets = [ "quantity" ]
+    static targets = [ "quantity", "sku", "addToCartButton" ]
+
+    add_to_cart(event){
+        event.preventDefault();
+        let product_id = this.data.get("id");
+        let quantity = this.quantityTarget.value;
+        let sku = this.skuTarget.value;
+        console.log(product_id, quantity, sku);
+        let data = new FormData();
+        data.append("id", product_id);
+        data.append("quantity", quantity);
+        data.append("sku", sku);
+        for (var value of data.values()) {
+            console.log(value);
+        }   
+
+        // Rails.ajax({
+        //     url: "api/hello",
+        //     type: 'POST',
+        //     data: data,
+        //     success: (resp) => {
+        //         console.log(resp);
+        //     },
+        //     error: (err) => {
+        //         console.log(err);
+        //     }
+        // })
+
+        if (quantity > 0) {
+            this.addToCartButtonTarget.classList.add('is-loading');
+        }
+    }
 
     quantity_minus(event){
         event.preventDefault();
@@ -18,5 +50,4 @@ export default class extends Controller {
         // console.log(value)
         this.quantityTarget.value = value + 1
     }
-
 }

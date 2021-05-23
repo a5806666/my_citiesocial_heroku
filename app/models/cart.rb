65cda733b1c3935481dec84f563cfae3039d9d74
zhong <afs784512@hotmail.com>
2021-05-23 14:47:45 +0900

    購物車 part 9(rspec)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,8 +5,26 @@ class Cart
     #  ↓
     attr_reader :items
 
-    def initialize
-        @items = []
+    def initialize(items = [])
+        @items = items
+    end
+
+    def self.from_hash(hash = nil)
+        # {
+        #     "items" => [
+        #       {"product_id" => 1, "quantity" =>3},
+        #       {"product_id" => 2, "quantity" =>2},
+        #     ]
+        # }
+
+        if hash && hash["items"]
+            items = hash["items"].map { |item|
+                CartItem.new(item["product_id"], item["quantity"])
+            }
+            Cart.new(items)
+        else
+            Cart.new    
+        end
     end
 
     def add_item(product_id)

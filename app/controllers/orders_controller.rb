b42911bb0cd97053b2bf95df724c96dbe9e9edea
zhong <afs784512@hotmail.com>
2021-05-25 17:30:52 +0900

    串接 LINE Pay part 3(gem_faraday(api) install)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,26 @@ class OrdersController < ApplicationController
 
     def create
         @order = current_user.orders.build(order_params)
-        
+
         current_cart.items.each do |item|
             @order.order_items.build(sku_id: item.sku_id, quantity: item.quantity)
         end
 
         if @order.save
+            # 打API(faraday)
+            resp = Faraday.post("#{ENV['line_pay_endpoint']}/v2/payments/request") do |req|
+                req.headers['Content-Type'] = 'application/json'
+                req.headers['X-LINE-ChannelId'] = ENV['line_pay_channel_id']
+                req.headers['X-LINE-ChannelSecret'] = ENV['line_pay_channel_secret']
+                req.body = {
+                    productName: "注文内容", 
+                    amount: current_cart.total_price.to_i, 
+                    currency: "TWD", 
+                    confirmUrl: "http://localhost:3000/orders/confirm", 
+                    orderId: @order.num
+                }.to_json
+            end
+            
             redirect_to root_path, notice: '支払いが完了しました'
         else
             render 'carts/checkout'
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
-
   validates :recipient, :tel, :address, presence: true
+
+  # 建立LINE PAY交易代碼
+  before_create :generate_order_num
+  private
+  def generate_order_num
+    self.num = SecureRandom.hex(5) unless num
+  end
 end
